a4fde2d21955915cd2d73c82aa1ad9a415d43c72
dev-sandakelum <dev.sandakelum@gmail.com>
2025-09-12 06:31:45 -0400

    tg2073 6

--- menu.c
+++ menu.c
@@ -13,6 +13,18 @@ int clear_screen() {
     }
 }
 
+int lines(int type) {
+    if(type == 1){
+        printf("----------------------------------------\n");
+    }else if (type == 2) {
+        printf("\n----------------------------------------\n");
+    }else if (type == 3) {
+        printf("========================================\n");
+    }else if (type == 4) {
+        printf("\n========================================\n");
+    }
+}
+
 int exit_to() {
     printf("Press any key to return to menu: ");
     char temp;
@@ -21,60 +33,94 @@ int exit_to() {
 
 int error_message(char *err) {
     if (err) {
-        printf("----------------------------------------\n");
+        lines(1);
         printf("%s\n", err);
-        printf("----------------------------------------\n");
+        lines(1);
     }
 }
 
 int top_bar(){
 
     clear_screen();
-    printf("\n========================================\n");
+    lines(4);
     printf("SRI LANKA PARLIAMENTARY ELECTION SYSTEM\n");
-    printf("----------------------------------------\n");
+    lines(1);
 }
 
 int register_user() {
     err = NULL;
+    //new variables
     int exit;
-    top_bar();
-    printf("| REGISTRATION -------------------------\n");
-
-    // Get user details
-    printf("  Enter your name: ");
+    char nic[20];
     char name[100];
-    scanf("%s", &name);
+    int dob , age;
+    char password[20] , confirm_password[20];
+    int section = 1;
 
-    // Get NIC number & validate
-    while(1){
-        error_message(err);
-        printf("  Enter your NIC number: ");
-        char nic[20];
-        scanf("%s", &nic);
-        if (strlen(nic) != 12) {
-            err = "Invalid NIC number.\nPlease enter a 12-digit NIC number.";
-            continue;
-        }else{
-            printf("\n| ELIGIBILITY -------------------------\n");
-            printf("  Enter your year of birth (YYYY): ");
-            int dob;
-            scanf("%d", &dob);
-            int age = 2025 - dob;
-            if (age >= 18) {
-                err = "Registration successful.";
+    while(section != 0){
+        top_bar(); //insert top bar
+        printf("| REGISTRATION -------------------------\n");
+
+        if(section == 1){
+            // Get user details
+            printf("  Enter your name: ");
+            scanf("%s", &name);
+
+            // Get NIC number & validate
+            while(1){
                 error_message(err);
-                exit_to();
-                break;
-            } else {
-                err = "User is not eligible to register.";
+                printf("  Enter your NIC number: ");
+                scanf("%s", &nic);
+                if (strlen(nic) != 12) {
+                    err = "Invalid NIC number.\nPlease enter a 12-digit NIC number.";
+                    continue;
+                }else{
+                    //DOB check but,only using birth year yet
+                    printf("\n| ELIGIBILITY -------------------------\n");
+                    printf("  Enter your year of birth (YYYY): ");
+                    scanf("%d", &dob);
+                    age = 2025 - dob;
+                    if (age >= 18) {
+                        section = 2;
+                        break;
+                    } else {
+                        err = "User is not eligible to register.";
+                        error_message(err);
+                        exit_to();
+                        break;
+                    }
+                }
+            }
+        }
+        else if(section == 2){
+
+            lines(1);
+            printf("You are eligible to register.\n");
+            lines(1);
+            printf("User name: %s\n", name);
+            printf("NIC number: %s\n", nic);
+            lines(3);
+
+            // password
+            printf("  Enter your password: ");
+            scanf("%c", password);
+            printf("  Confirm your password: ");
+            scanf("%c", confirm_password);
+
+            if (password != confirm_password) {
+                err = "Passwords do not match.";
                 error_message(err);
+            
+                lines(1);
+                printf("Error: Password attempts exceeded. \nExiting after 1 failed tries.\n");
+                lines(3);
                 exit_to();
                 break;
             }
         }
+        
     }
-    return 1;
+        return 1;
 }
 
 int main() {
@@ -86,7 +132,7 @@ int main() {
         printf("3. Vote\n");
         printf("4. View Results\n");
         printf("0. Exit\n");
-        printf("========================================\n");
+        lines(3);
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -113,15 +159,11 @@ int main() {
                 printf("Invalid choice. Please try again.\n");
                 break;
         }
-        if (key_return == 1) {
-            printf("========================================\n");
-            printf("You must be at least 18 years old\n");
-            printf("========================================\n");
-            
-        }else if(key_return == 5){
-            printf("========================================\n");
+       
+        if(key_return == 5){
+            lines(3);
             printf("Successfully exited\n");
-            printf("========================================\n");
+            lines(3);
             break;
         }
     }
