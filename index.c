f02b089fe7d7deba6ad94f531b0db54ee67590db
dev.sandakelum <dev.sandakelum@example.com>
2025-10-03 21:42:20 +0530

    Enhance user login functionality: update user structure, implement login by password, and improve menu prompts

--- index.c
+++ index.c
@@ -10,18 +10,27 @@ int the_front();
 int welcome_screen();
 int register_user();
 int login_user();
-int vote_user();
+int vote_user(char *user_nic);
 int view_results();
 int main_menu(char *user_nic);
 int save_user_as_voter(char *nic, char *name, char *password, int age );
 int save_user_as_candidate(char *nic, char *name, char *password, int age , int district, int party);
 int check_nic_exists(char *nic);
 char *find_nic(char *nic);
-char *login_user_data(char *nic, char *password);
+int login_by_pass(char *nic ,char *password);
+//char *login_user_data(char *nic, char *password);
 
 // Global variables
 char *err;
-char *user[] = {"200334455667", NULL, NULL, NULL}; // nic, name, password, age
+char user[4][100] = {"", "", "", ""}; // nic, name, password, age
+
+int set_user(char *nic, char *name, char *password, char *age) {
+    strcpy(user[0], nic);
+    strcpy(user[1], name);
+    strcpy(user[2], password);
+    strcpy(user[3], age);
+    return 0;
+}
 
 int main() {
     
@@ -41,3 +50,4 @@ int main() {
 #include "results.c"
 #include "menu.c"
 #include "file_handle_register.c"
+#include "file_handle_login.c"
--- login.c
+++ login.c
@@ -1,73 +1,9 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include "fun.h"
 
-void login_details_section(char *nic, char *name) {
-    top_bar();
-    printf("| LOGIN DETAILS -----------------------\n");
-    color_text(3);
-    printf("NIC: %s\n", nic);
-    printf("Name: %s\n", name);
-    color_text(0);
-    lines(1);
-}
-char *send[4] = {NULL, NULL, NULL, NULL}; // nic, name, password, age
-int login_user() {
-    char *err = NULL;
-    char nic[20] = "";
-    char password[20] = "";
-    char name[100] = "";
-    int attempts = 3;
-    char *user_details; 
-
-    while (attempts > 0) {
-        //top_bar();
-        printf("| USER LOGIN -------------------------\n");
-        color_text(3);
-        
-        // Get NIC
-        printf("Enter NIC Number (0 to exit): ");
-        scanf("%s", nic);
-        if (exit_from_0(*nic, 0)) {
-            return 0;
-        }
-
-        // Get Password
-        printf("Enter Password (0 to exit): ");
-        scanf("%s", password);
-        if (exit_from_0(*password, 0)) {
-            return 0;
-        }
-
-        user_details = login_user_data(nic, password);
-        color_text(0);
-
-        if (user_details == NULL) {
-            err = "NIC number not found";
-            error_message(err);
-            attempts--;
-            if (attempts > 0 && try_again() == 0) {
-                continue;
-            }
-            break;
-        } else {
-            char *db_nic = strtok(user_details, ",");
-            char *db_name = strtok(NULL, ",");
-            char *db_password = strtok(NULL, ",");
-            char *db_age = strtok(NULL, ",");
-            printf("user_details: %s\n", db_nic);
-            printf("user_details: %s\n", db_name);
-            printf("user_details: %s\n", db_password);
-            printf("user_details: %s\n", db_age);
-        }
-
-        free(user_details);
-    }
-
-    if (attempts == 0) {
-        error_message("Too many failed attempts");
-    }
-    exit_to();
+int login_user()
+{
     return 0;
 }
+
