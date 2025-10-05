e4c3075e3a9d97a344b296560b68afa0cf62217d
dev.sandakelum <dev.sandakelum@example.com>
2025-10-05 14:36:04 +0530

    Refactor login_user function: improve code structure and enhance user feedback during login process

--- file_handle_login.c
+++ file_handle_login.c
@@ -7,6 +7,22 @@
 //--------------------------------------------------------------------------------------------------
 
 int login_by_pass(char *nic ,char *password , char log_type[10]){
-    
+    FILE *user_text = fopen("data/users.txt", "r");
+    if (user_text == NULL)
+    {
+        return 2;
+    }
+    char line[256] , db_nic[12] , db_name[50] , db_password[30] , db_age[3];
+    while (fgets(line, sizeof(line), user_text))
+    {
+        sscanf(line, "%[^,],%[^,],%[^,],%[^,]", db_nic ,db_name, db_password , db_age );
+        if(strcmp(db_nic , nic)== 0 && (strcmp(db_password , password)== 0 || log_type == "session")){
+            save_login_session(db_nic);
+            set_user(db_nic, db_name, db_password, db_age);
+            fclose(user_text);
+            return 1; // success
+        }
+    }
+    fclose(user_text);
     return 0;
 }
--- login.c
+++ login.c
@@ -4,7 +4,37 @@
 
 int login_user()
 {
-    
+    top_bar();
+    char nic[20] = "";
+    char password[20] = "";
+
+    printf("NIC : ");
+    scanf("%s", nic);
+
+    printf("Password : ");
+    scanf("%s", password);
+
+    //------- Check credentials from file
+    int result = login_by_pass(nic, password ,"password"); // nic , password , login_type
+    if (result == 1)
+    {
+        color_text(2);
+        printf("Login successful! Welcome, %s.\n", nic);
+    }
+    else if (result == 0)
+    {
+        color_text(1);
+        printf("Login failed! \nInvalid NIC or password.\n");
+    }
+    else
+    {
+        color_text(1);
+        printf("Error opening user data file.\n");
+        return 2;
+    }
+    color_text(0);
+    lines(1);
+    exit_to();
     return 0;
 }
 
